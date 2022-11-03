bcf1391d52bafd7d4f1d0842e6707525a70d042c
Ohenebaa <hansopoku360@gmail.com>
2022-11-03 17:48:10 +0000

    mod4 final

---
+++ Mod4-Assignment/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ Mod4-Assignment/SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var speakWord = "Hello";
+  var helloSpeaker = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
